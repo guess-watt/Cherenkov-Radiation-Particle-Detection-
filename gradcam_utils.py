8dcd0224c3071ac12dbebf3ebbdf3a0b823caa5e
AWN <anwinasprogramer@gmail.com>
2026-03-07 23:48:42 +0530

    Add files via upload

---
+++ gradcam_utils.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+IMG_SIZE = 224
+
+def make_gradcam(model, img_array, last_conv_layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_output, preds = grad_model(img_array)
+        class_idx = tf.argmax(preds[0])
+        loss = preds[:, class_idx]
+
+    grads = tape.gradient(loss, conv_output)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    conv_output = conv_output[0]
+    heatmap = tf.reduce_sum(conv_output * pooled_grads, axis=-1)
+
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap) if np.max(heatmap) != 0 else 1
+
+    return heatmap
