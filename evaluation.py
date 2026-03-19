aeb0eb2b1979e162a79e2cfab4498b5c40481f53
AWN <anwinasprogramer@gmail.com>
2026-03-19 23:25:51 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,6 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 
-
 # Load trained model
 model = tf.keras.models.load_model("particle_classifier.h5")
 # (Use this because I can see this file in your folder)
